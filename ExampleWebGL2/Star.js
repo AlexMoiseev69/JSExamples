6151e68c35a9fa923dec09009a86bf30b32aad27
Alexander <amoiseev@live.com>
2016-11-23 19:20:31 +0300

    examples

---
+++ ExampleWebGL2/Star.js
@@ -0,0 +1,100 @@
+/**
+ * Created by user on 15.09.2016.
+ */
+
+function Star(startingDistance, rotationSpeed) {
+    this.angle = 0;
+    this.dist = startingDistance;
+    this.rotationSpeed = rotationSpeed;
+    // Set the colors to a starting value.
+    this.randomiseColors();
+}
+
+Star.prototype.draw = function (tiltY, tiltX, spin, twinkle) {
+    mvPushMatrix();
+    // Move to the star's position
+    mat4.rotate(mvMatrix, degToRad(this.angle), [0.0, 1.0, 0.0]);
+    mat4.translate(mvMatrix, [this.dist, 0.0, 0.0]);
+    // Rotate back so that the star is facing the viewer
+    mat4.rotate(mvMatrix, degToRad(-this.angle), [0.0, 1.0, 0.0]);
+    mat4.rotate(mvMatrix, degToRad(-tiltY), [1.0, 0.0, 0.0]);
+    mat4.rotate(mvMatrix, degToRad(-tiltX), [0.0, 1.0, 0.0]);
+    if (twinkle) {
+        // Draw a non-rotating star in the alternate "twinkling" color
+        gl.uniform3f(shaderProgram.colorUniform, this.twinkleR, this.twinkleG, this.twinkleB);
+        drawStar();
+    }
+    // All stars spin around the Z axis at the same rate
+    mat4.rotate(mvMatrix, degToRad(spin), [0.0, 0.0, 1.0]);
+    // Draw the star in its main color
+    gl.uniform3f(shaderProgram.colorUniform, this.r, this.g, this.b);
+    drawStar()
+    mvPopMatrix();
+};
+
+var effectiveFPMS = 60 / 1000;
+Star.prototype.animate = function (elapsedTime) {
+    this.angle += this.rotationSpeed * effectiveFPMS * elapsedTime;
+    // Decrease the distance, resetting the star to the outside of
+    // the spiral if it's at the center.
+    this.dist -= 0.01 * effectiveFPMS * elapsedTime;
+    if (this.dist < 0.0) {
+        this.dist += 5.0;
+        this.randomiseColors();
+    }
+};
+
+Star.prototype.randomiseColors = function () {
+    // Give the star a random color for normal
+    // circumstances...
+    this.r = Math.random();
+    this.g = Math.random();
+    this.b = Math.random();
+    // When the star is twinkling, we draw it twice, once
+    // in the color below (not spinning) and then once in the
+    // main color defined above.
+    this.twinkleR = Math.random();
+    this.twinkleG = Math.random();
+    this.twinkleB = Math.random();
+};
+
+var mvMatrix = mat4.create();
+var mvMatrixStack = [];
+var pMatrix = mat4.create();
+function mvPushMatrix() {
+    var copy = mat4.create();
+    mat4.set(mvMatrix, copy);
+    mvMatrixStack.push(copy);
+}
+function mvPopMatrix() {
+    if (mvMatrixStack.length == 0) {
+        throw "Invalid popMatrix!";
+    }
+    mvMatrix = mvMatrixStack.pop();
+}
+function setMatrixUniforms() {
+    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+
+function handleLoadedTexture(texture) {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function drawStar() {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, starTexture);
+    gl.uniform1i(shaderProgram.samplerUniform, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, starVertexTextureCoordBuffer);
+    gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, starVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, starVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, starVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, starVertexPositionBuffer.numItems);
+}
